93066c67633ca5ef4d0fb1210fec62287ae840d9
calumgould <calumgould26@gmail.com>
2025-05-03 18:29:53 +0100

    Add socketlib and trigger damage calc macro on chat message

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -1,26 +1,49 @@
+let socket: any
+
 Hooks.on('ready', () => {
   console.log('Star Wars FFG addon module loaded')
 })
 
-Hooks.on('createChatMessage', async (message: any) => {
-  console.log('Chat message created:', message)
+Hooks.once('socketlib.ready', () => {
+  socket = socketlib.registerModule('star-wars-ffg-addon');
 
-  const macroName = 'Damage Calculator'
-  const compendiumName = 'star-wars-ffg-addon.star-wars-ffg-addon-macros'
+  // Register a callable function that runs the macro
+  socket.register('runDamageCalculatorMacro', async () => {
+    const macroName = 'Damage Calculator'
+    const compendiumName = 'star-wars-ffg-addon.star-wars-ffg-addon-macros';
+    const compendium = game.packs.get(compendiumName);
 
-  const compendium = game.packs.get(compendiumName)
-
-  if (compendium) {
-    const documents = await compendium.getDocuments()
-    console.log('Compendium documents:', documents)
-    const macro = documents.find((entry: any) => entry.name === macroName)
-    console.log('Macro found:', macro)
-
-    if (macro) {
-      console.log('Executing macro:', macro)
-      await macro.execute()
-    } else {
-      console.error(`Macro "${macroName}" not found in compendium "${compendiumName}"`)
+    if (!compendium) {
+      console.error(`Compendium "${compendiumName}" not found`);
+      return;
     }
-  }
-})
+
+    const documents = await compendium.getDocuments();
+    const macro = documents.find((entry: any) => entry.name === macroName);
+
+    if (!macro) {
+      console.error(`Macro "${macroName}" not found in compendium`);
+      return;
+    }
+
+    await macro.execute();
+  });
+
+  Hooks.on('createChatMessage', async (message: any) => {
+    console.log('Chat message created:', message);
+
+    const attackKeywords = ['RangedHeavy', 'RangedLight', 'Melee', 'Lightsaber', 'Gunnery', 'Brawl']
+
+    if (
+      attackKeywords.some((keyword) => message.flavor.replace(/[:\s]/g, '').includes(keyword)) &&
+      message.rolls.length &&
+      message.rolls[0].data.type === 'weapon'
+    ) {
+      await socket.executeAsGM('runDamageCalculatorMacro');
+    } else {
+      console.log("Chat message is not an attack so there is nothing to do")
+    }
+
+  });
+});
+
